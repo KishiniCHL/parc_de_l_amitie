8c081d5c06dd8e5abca9d67ea290e431ed880bab
Hoai-lan DINH VU <christiane.dinhvu@gmail.com>
2022-10-22 16:43:30 +0200

    button up et nav

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let arrow_top = document.getElementsByClassName("arrow_top")[0]
 let arrow_bot = document.getElementsByClassName('arrow_bottom')[0]
-
+let up_button = document.getElementById("up_button")
+let navbar = document.getElementById("navbar")
 let first_section = document.getElementById("first_content")
 let second_section = document.getElementById("second_content")
 let third_section = document.getElementById("third_content")
@@ -14,6 +15,7 @@ function scroll_function(){
     }  
     else{
         arrow_top.style.visibility = "";
+
     } 
 
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
@@ -21,14 +23,31 @@ function scroll_function(){
     }
     else{
         arrow_bot.style.visibility = "";
+    };
+}
+
+function hide_nav(){
+    
+    if(((window.innerHeight + window.scrollY) >= document.body.offsetHeight) && document.body.clientWidth <= 1024){
+        navbar.style.visibility = "hidden";
+        up_button.style.visibility = "visible";
     }
+    else{
+        navbar.style.visibility = "visible";
+        up_button.style.visibility = "hidden";
+    };
 }
 
 
 
+window.onscroll = function () {
+    scroll_function()
+    hide_nav()
+}
 
-window.onscroll = function () {scroll_function()
-
+window.onresize = function () {
+    hide_nav()
+    scroll_function()
 }
 
 const array = ['first_section','second_section', 'third_section', 'fourth_section' ]
@@ -37,13 +56,24 @@ array.forEach((zone) => {
     
 });
 
+var rootElement = document.documentElement
+
+function go_top() {
+    rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+    });
+}
+
+up_button.addEventListener("click", go_top)
+
 
 const position = document.querySelector('section')
 const getOffset = (el) => {
-  const rect = el.getBoundingClientRect();
-  return {
+const rect = el.getBoundingClientRect();
+return {
     left: rect.left + window.scrollX,
     top: rect.top + window.scrollY
-  };
+};
 }
 console.log(getOffset(section));
